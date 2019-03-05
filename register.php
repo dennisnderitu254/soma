109ddf4aaa51d725f74d7715f36c29ad8dcd178b
dennisnderitu254 <dknderitu@gmail.com>
2019-03-05 19:16:55 +0300

    First Commit

---
+++ register.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Soma App</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/heroic-features.css" rel="stylesheet">
+
+</head>
+    <body class="container">
+        <h2>Registration Page</h2>
+        <a href="index.php">Click here to go back<br/><br/>
+        <form class = "container" role="form">
+
+          <div class="form-group">
+                <label>Registration Number:</label>
+                <input  class="form-control" id="registrationumber">
+             </div>
+             <div class="form-group">
+                <label>Firstname:</label>
+                <input  class="form-control" id="firstname">
+             </div>
+             <div class="form-group">
+                <label>Lastname:</label>
+                <input  class="form-control" id="lastname">
+             </div>
+                <div class="form-group">
+                  <label>Password:</label>
+                  <input type="password" class="form-control" id="pwd">
+                </div>
+                      <div class="checkbox">
+                        <label><input type="checkbox"> Remember me</label>
+                      </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+    </body>
+</html>
